603c91fe31bc976bd65198c7234dfeb0dec974d3
cwmcnutt <64500902+cwmcnutt@users.noreply.github.com>
2024-01-14 13:20:29 -0600

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -1,1 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello Firestarter!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
+
+#curl -v -G http://localhost:105/hello/
 
